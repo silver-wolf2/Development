e02a5f28c39eea426d500fb86d353e1b0551bd86
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-01-02 22:10:26 +0200

    create and deploy test

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,17 +3,16 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
-    uint256 number = 1;
+    FundMe fundMe;
 
     function setUp() external {
-        number = 2;
+        fundMe = new FundMe();
     }
 
-    function testDemo() public {
-        console.log(number);
-        console.log("Hello, World!");
-        assertEq(number, 2);
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
 }
