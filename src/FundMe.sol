1f8c5baf2900e366b1d04a4713614b09dcd40989
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-01-08 15:57:43 +0200

    Format the code

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.18;
 
 import {PriceConverter} from "./PriceConverter.sol";
-import {AggregatorV3Interface} from "chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from
+    "chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 error FundMe_NotOwner();
 
--- test/mocks/MockV3Aggregator.sol
+++ test/mocks/MockV3Aggregator.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AggregatorV3Interface} from "chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from
+    "chainlink-brownie-contracts/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 /**
  * @title MockV3Aggregator
@@ -33,58 +34,28 @@ contract MockV3Aggregator is AggregatorV3Interface {
         startedAt[latestRound] = block.timestamp;
     }
 
-    function updateRoundData(
-        uint80 _roundId,
-        int256 _answer,
-        uint256 _timestamp
-    ) public {
+    function updateRoundData(uint80 _roundId, int256 _answer, uint256 _timestamp) public {
         answers[_roundId] = _answer;
         timestamps[_roundId] = _timestamp;
     }
 
-    function getRoundData(
-        uint80 _roundId
-    )
+    function getRoundData(uint80 _roundId)
         public
         view
         override
-        returns (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAtTime,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        )
+        returns (uint80 roundId, int256 answer, uint256 startedAtTime, uint256 updatedAt, uint80 answeredInRound)
     {
         require(_roundId <= latestRound, "No data present");
-        return (
-            _roundId,
-            answers[_roundId],
-            startedAt[_roundId],
-            timestamps[_roundId],
-            _roundId
-        );
+        return (_roundId, answers[_roundId], startedAt[_roundId], timestamps[_roundId], _roundId);
     }
 
     function latestRoundData()
         public
         view
         override
-        returns (
-            uint80 roundId,
-            int256 answer,
-            uint256 startedAtTime,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        )
+        returns (uint80 roundId, int256 answer, uint256 startedAtTime, uint256 updatedAt, uint80 answeredInRound)
     {
-        return (
-            uint80(latestRound),
-            latestAnswer,
-            startedAt[latestRound],
-            latestTimestamp,
-            uint80(latestRound)
-        );
+        return (uint80(latestRound), latestAnswer, startedAt[latestRound], latestTimestamp, uint80(latestRound));
     }
 
     function description() public pure override returns (string memory) {
