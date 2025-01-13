1f2135e212fa2070cd110491172c2553d39ba6ff
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-01-13 10:02:16 +0200

    Implenent the starting Raffe contract

--- Lottery/src/contracts/Raffle.sol
+++ Lottery/src/contracts/Raffle.sol
@@ -1,5 +1,29 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
 
-contract Raffle {}
+/**
+ * @title A sample Raffle contract
+ * @author Vladimir Panev
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2.5
+ */
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {}
+
+    function pickWinner() public {}
+
+    /**
+     * Getter function for entrance fee
+     */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
