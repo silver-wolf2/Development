408f611611db6ea536ef2d666d06fca04ee7d48f
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-01-13 14:17:18 +0200

    Implement a file structure and custom error

--- Lottery/src/contracts/Raffle.sol
+++ Lottery/src/contracts/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external functions
+// public functions
+// internal functions
+// private functions
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
@@ -10,13 +31,20 @@ pragma solidity ^0.8.19;
  */
 
 contract Raffle {
+    /* Errors */
+    error Raffle_SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_SendMoreToEnterRaffle();
+        }
+    }
 
     function pickWinner() public {}
 
