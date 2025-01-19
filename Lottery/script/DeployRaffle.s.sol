6c371c5513d29ce1c852ee4aa0debdf791d7370b
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-01-19 10:02:43 +0200

    Implement deplly script and Helper

---
+++ Lottery/script/DeployRaffle.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "../src/contracts/Raffle.sol";
+
+//Lottery/src/contracts/Raffle.sol
+
+contract DeplouRaffle is Script {
+    function run() public {}
+}
