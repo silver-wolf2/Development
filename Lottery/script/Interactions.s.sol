737ed41c3427415fb687939d68e3bd3787235490
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-01-20 13:56:07 +0200

    Implement interactions for subscription

---
+++ Lottery/script/Interactions.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+contract CreateSubscriptions is Script {
+    function createSubscriptionConfig() public returns (uint256, address) {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        (uint256 subId, ) = createSubscription(vrfCoordinator);
+        return (subId, vrfCoordinator);
+    }
+
+    function createSubscription(
+        address vrfCoordinator
+    ) public returns (uint256, address) {
+        console.log("Creating subscription on chain Id: ", block.chainid);
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator)
+            .createSubscription();
+        vm.stopBroadcast();
+
+        console.log("Your subscription Id is: ", subId);
+        console.log(
+            "Please update the subscription Id in the HelperConfig contract"
+        );
+        return (subId, vrfCoordinator);
+    }
+
+    function run() public {}
+}
