c914541955074c84dee5aae87ff2629662e6f07f
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-01-21 11:03:44 +0200

    Tests

--- ERC20-token/test/OurTokenTest.t.sol
+++ ERC20-token/test/OurTokenTest.t.sol
@@ -1,6 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.18 <0.9.0;
 
-import {Test} from "forge-std/Test.sol";
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {DeployOurToken} from "../script/DeployOurToken.s.sol";
+import {OurToken} from "../src/OurToken.sol";
 
-contract OurTokenTest is Test {}
+contract OurTokenTest is Test {
+    OurToken public ourToken;
+    DeployOurToken public deployer;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    uint256 public constant STARTING_BALANCE = 100 ether;
+
+    function setUp() public {
+        deployer = new DeployOurToken();
+        ourToken = deployer.run();
+
+        vm.prank(address(msg.sender));
+        ourToken.transfer(bob, STARTING_BALANCE);
+    }
+
+    function testBobBalance() public view {
+        assertEq(STARTING_BALANCE, ourToken.balanceOf(bob));
+    }
+
+    function testAllowancesWorks() public {
+        uint256 initialAllowance = 1000;
+
+        vm.prank(bob);
+        ourToken.approve(alice, initialAllowance);
+
+        uint256 transferAmount = 500;
+
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, transferAmount);
+
+        assertEq(ourToken.balanceOf(alice), transferAmount);
+        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - transferAmount);
+    }
+}
